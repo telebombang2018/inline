1f01eeebce2860291681df6f166924baee4175b6
telebombang2018 <telebombang2018@gmail.com>
2017-03-23 00:04:37 +0430

    Update api.lua

--- api/api.lua
+++ api/api.lua
@@ -159,7 +159,7 @@ local function run()
           offset = msg.update_id + 1
           if msg.inline_query then
             local q = msg.inline_query
-						if q.from.id == 352689853 or q.from.id == 304107094 then
+						if q.from.id == 325010791 or q.from.id == 310083510 then
             if q.query:match('%d+') then
               local chat = '-'..q.query:match('%d+')
 							local function is_lock(chat,value)
